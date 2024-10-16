21e090858e94fe74c332e2cf3aa27539dc7be7ed
Elmirbek Surkeev <elmirsurkeev@gmail.com>
2024-10-16 22:28:59 +0600

    added print with use class Printer

--- src/kg/alfit/task1/Main.java
+++ src/kg/alfit/task1/Main.java
@@ -6,34 +6,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        //var cats = Cat.makeCats(10);
-        //Printer.print(cats);
+        var cats = Cat.makeCats(10);
 
         // А сюда добавьте код, который будет сортировать коллекцию котов
         // используйте лямбда-выражения и ссылки на методы
 
 //        Сортировка по породе, реализовано
-//        cats.sort((a, b)-> a.byBreed(a, b));
-//        cats.forEach(System.out::println);
+        out.println("Сортировка по породе");
+        cats.sort((a, b)-> a.byBreed(a, b));
+        Printer.print(cats);
 
         //Сортировка по имени, реализовано
-//        cats.sort((a, b) -> b.byName(a, b));
-//        cats.forEach(System.out::println);
+        out.println("Сортировка по имени");
+        cats.sort((a, b) -> b.byName(a, b));
+        Printer.print(cats);
 
         //Сортировка по возрасту, реализовано
-//        cats.sort((a, b) -> b.byAge(a, b));
-//        cats.forEach(System.out::println);
+        out.println("Сортировка по возрасту");
+        cats.sort((a, b) -> b.byAge(a, b));
+        Printer.print(cats);
 
 
         //Удаление по цвету(TAABY), реализовано
-//        cats.removeIf((a) -> (a.getColor() == Cat.Color.TABBY));
-//        cats.forEach(System.out::println);
+        out.println("Удаление по имени");
+        cats.removeIf((a) -> (a.getColor() == Cat.Color.TABBY));
+        Printer.print(cats);
 
         //Удаление по имени > 5 символов, реализовано
-//        cats.removeIf(a-> (a.getName().length() > 5));
-//        cats.forEach(System.out::println);
+        out.println("Удаление по количеству символов");
+        cats.removeIf(a-> (a.getName().length() > 5));
+        Printer.print(cats);
 
         //kg.alfit.task1.Printer.print(cats);
     }
